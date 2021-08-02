de5c8a253ed39cd5e9d858b5e1aac297db758547
michelleamesquita <44561732+michelleamesquita@users.noreply.github.com>
2021-08-02 11:13:27 -0300

    Update google.js

--- cypress/integration/Google/google.js
+++ cypress/integration/Google/google.js
@@ -1,8 +1,8 @@
 import {Given,Then} from 'cypress-cucumber-preprocessor/steps'
 // import GoogleSearchPage from './searchPage'
 
-//const url = 'https://google.com'
-const url = 'teste'
+const url = 'https://google.com'
+
 Given('I open Google page', () => {
   cy.visit(url)
 })
